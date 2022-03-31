56d521c971ca43b8f113b54734ed9fc3127f4ece
Lakota Rose <itsrosebro@gmail.com>
2022-03-31 16:37:55 -0500

    Added admin file with mysql, Updated views.py + setting.py files.

--- products/views.py
+++ products/views.py
@@ -4,7 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-import products
 from .serializers import ProductSerializer
 from .models import Product
 from products import serializers
