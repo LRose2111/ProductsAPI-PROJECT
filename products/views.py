21301c509b78990853ea1dc5bf74ca374a37d81f
Lakota Rose <itsrosebro@gmail.com>
2022-03-31 17:16:26 -0500

    Updated Views.py file.

--- products/views.py
+++ products/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
 from .serializers import ProductSerializer
 from .models import Product
-from products import serializers
+
 
 @api_view(['GET', 'POST'])
 def products_list(request):
@@ -29,7 +28,7 @@ def product_detail(request, pk):
         product = get_object_or_404(Product, pk=pk)
         if request.method == 'GET':
             
-            serializer = Product(product);
+            serializer = ProductSerializer(product);
             return Response(serializer.data)
         elif request.method == 'PUT':
             
